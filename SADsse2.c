168ddeaefef9e6f65ead8c755d46d56dd9f2e697
sinusee <sinusee@gmail.com>
2017-02-05 21:31:57 +0330

    Update SADsse2.c

--- SADsse2.c
+++ SADsse2.c
@@ -19,38 +19,18 @@ int main()
 	int i, j, w=0, sad=0;
 	struct timespec tStart, tEnd;
  	double tTotal , tBest=10000;
- 	//__m128i sad_temp;
  	__m128i vec1, vec2, vecT, sad_total;
  	sad_total= _mm_setzero_si128();
  	
 	do{
 		clock_gettime(CLOCK_MONOTONIC,&tStart);
-	 
-		
-		
-		
 		for(i=0; i<MAX1; i++){
-			//sad_temp= _mm_setzero_si128();
 			for(j=0; j<MAX2; j+=16){//IACA_START
 					
-					//SAD3
-					//sad_temp = _mm_sub_epi8 ( _mm_load_si128((__m128i *)&a_char[i][j]) ,  _mm_load_si128((__m128i *)&b_char[i][j]));
-					//sad_temp = _mm_abs_epi8(sad_temp);
-					//sad_total = _mm_add_epi16 ( sad_temp , sad_total);
-					
-					//SAD2
-					//sad_temp = _mm_sub_epi8 ( _mm_load_si128((__m128i *)&a_char[i][j]) ,  _mm_load_si128((__m128i *)&b_char[i][j]));
-					//sad_temp = _mm_sign_epi8(sad_temp , sad_temp);
-					//sad_total = _mm_add_epi16 ( sad_temp , sad_total);
-					
-					
-					//SAD1
-					//sad_temp  = _mm_sad_epu8 ( _mm_load_si128((__m128i *)&a_char[i][j]) ,  _mm_load_si128((__m128i *)&b_char[i][j]));
-					vec1 = _mm_load_si128((__m128i *)&a_char[i][j]);
-					vec2 = _mm_load_si128((__m128i *)&b_char[i][j]);
-					vecT = _mm_sad_epu8( vec1 , vec2);
-					sad_total = _mm_add_epi16(vecT, sad_total);
-					//sad_total = _mm_add_epi16 ( _mm_sad_epu8 ( _mm_load_si128((__m128i *)&a_char[i][j]) ,  _mm_load_si128((__m128i *)&b_char[i][j])) , sad_total);
+				vec1 = _mm_load_si128((__m128i *)&a_char[i][j]);
+				vec2 = _mm_load_si128((__m128i *)&b_char[i][j]);
+				vecT = _mm_sad_epu8( vec1 , vec2);
+				sad_total = _mm_add_epi64(vecT, sad_total);
 
 				}//IACA_END
 			}
